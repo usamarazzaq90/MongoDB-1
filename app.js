3a8afb1c254c6e985d4c6ec915a09f6455f27b7f
usamarazzaq90 <usamarazzaq9014@gmail.com>
2024-10-30 11:38:27 +0500

    Completed Part 12

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const app=express()
 const path=require('path')
+const userModel=require('./models/user')
 
 app.set("view engine","ejs")
 app.use(express.json())
@@ -11,11 +12,29 @@ app.get('/',(req,res)=>{
     res.render("index")
 })
 
-app.get('/read',(req,res)=>{
-    res.render("read")
+app.post('/create',async (req,res)=>{
+    let {name, email, image}=req.body
+    let createdUser= await userModel.create({
+        // name: name,
+        // email:email,
+        // image: image
+        // or simply
+        name,
+        email,
+        image 
+    })
+
+    res.redirect('/read')
 })
 
-app.post('/cretae',(req,res)=>{
+app.get('/read',async (req,res)=>{
+    let users= await userModel.find();
+    res.render("read",{users})
+})
+
+app.get('/delete/:id',async (req,res)=>{
+    let users= await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/read")
 })
 
 app.listen(3000);
